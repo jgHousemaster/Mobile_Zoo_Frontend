fb75b6f781d96b0db5feda113dd6b787e254158c
cxhZCM123 <931736813@qq.com>
2019-06-22 00:17:00 +0800

    new info

--- pages/animalInfo/index.js
+++ pages/animalInfo/index.js
@@ -1,5 +1,5 @@
 // pages/animalInfo/index.js
-const app = getApp();;
+const app = getApp();
 
 Page({
 
@@ -9,7 +9,8 @@ Page({
   data: {
     current: "result",
     picUrl: {},
-    speciesResult: {}
+    speciesResult: {},
+    speciesType: -1
   },
   handleChange({ detail }) {
     this.setData({
@@ -20,13 +21,47 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var result = "" //储存处理后的识别结果
+    var type = -1
     if(app.globalData.speciesResult.length === 0){
-      app.globalData.speciesResult = "无物种识别结果"
+      result += "无物种识别结果"
+    }
+    else{
+      var speciesString = app.globalData.speciesResult.split('\n')
+      for (var i = 0; i < speciesString.length; i++) {
+        var index = speciesString[i].indexOf(":")
+        if (index === -1)
+          continue
+        var name = speciesString[i].slice(0, index)
+        if (name === "panda") {
+          speciesString[i] = "大熊猫" + speciesString[i].slice(index)
+          type = 0
+        }
+        else if (name === "lesser panda") {
+          speciesString[i] = "小熊猫" + speciesString[i].slice(index)
+          type = 1
+        }
+        else if (name === "peacock") {
+          speciesString[i] = "孔雀" + speciesString[i].slice(index)
+          type = 2
+        }
+        else if (name === "swan") {
+          speciesString[i] = "天鹅" + speciesString[i].slice(index)
+          type = 3
+        }
+        else if (name === "white crane") {
+          speciesString[i] = "白鹤" + speciesString[i].slice(index)
+          type = 4
+        }
+        result += speciesString[i] + "\n"
+      }
     }
     this.setData({
      picUrl: 'https://zoo.scubrl.org/getPic/' + JSON.stringify(app.globalData.globalId).slice(1, -1) + '/',
-      speciesResult: app.globalData.speciesResult
+      speciesResult: result,
+      speciesType: type
     })
+    
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
